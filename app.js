bceec9c61e81b9361a5760598972decee5256b86
Mark Verbeek <markverbeek@mac.com>
2020-07-10 15:24:32 +0200

    added styling to show page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose")
 var Campground = require("./models/campground")
+var Comment = require("./models/comment")
 var seedDB = require("./seeds")
 
 seedDB();
@@ -11,6 +12,7 @@ mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true,
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 
 
@@ -39,7 +41,7 @@ app.get("/campgrounds", function (req, res) {
     if (err) {
       console.log(err);
     } else {
-      res.render("index", { campgrounds: allCampgrounds });
+      res.render("campgrounds/index", { campgrounds: allCampgrounds });
     }
   })
 
@@ -66,7 +68,7 @@ app.post("/campgrounds", function (req, res) {
 //NEW ROUTE
 
 app.get("/campgrounds/new", function (req, res) {
-  res.render("new.ejs");
+  res.render("campgrounds/new");
 });
 
 
@@ -78,13 +80,47 @@ app.get("/campgrounds/:id", function (req, res) {
       console.log(err);
     } else {
       console.log(foundCampground)
-      res.render("show", { campground: foundCampground });
+      res.render("campgrounds/show", { campground: foundCampground });
 
     }
   });
+});
 
+//==================
+// COMMENTS ROUTES
+//==================
+
+app.get("/campgrounds/:id/comments/new", function (req, res) {
+  Campground.findById(req.params.id, function (err, campground) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("comments/new", { campground: campground });
+    }
+  })
+});
+
+app.post("/campgrounds/:id/comments", function (req, res) {
+  Campground.findById(req.params.id, function (err, campground) {
+    if (err) {
+      console.log(err);
+      res.redirect("/campgrounds");
+    } else {
+      Comment.create(req.body.comment, function (err, comment) {
+        if (err) {
+          console.log(err);
+        } else {
+          campground.comments.push(comment);
+          campground.save();
+          res.redirect('/campgrounds/' + campground._id);
+        }
+      })
+    }
+  })
 
 })
+
+
 
 
 
