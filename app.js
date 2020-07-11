87d05c7ed05f14f9b02fe9dfc18ab9f61561ccee
Mark Verbeek <markverbeek@mac.com>
2020-07-11 17:27:38 +0200

    added passport

--- app.js
+++ app.js
@@ -2,17 +2,33 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose")
+var passport = require("passport")
+var LocalStrategy = require("passport-local")
 var Campground = require("./models/campground")
 var Comment = require("./models/comment")
+var User = require("./models/user")
 var seedDB = require("./seeds")
 
-seedDB();
+
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true, useUnifiedTopology: true })
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+seedDB();
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+  secret: "Once again, Rusty wins",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 
 
@@ -124,8 +140,14 @@ app.post("/campgrounds/:id/comments", function (req, res) {
 
 
 
+//=============
+// AUTH ROUTES
+//==============
 
 
+app.get("/register", function (req, res) {
+  res.render("register");
+})
 
 
 
