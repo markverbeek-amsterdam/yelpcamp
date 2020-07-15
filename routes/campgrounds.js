26bfcf4027d3f5f753cf276cf76cc61751aaa88e
Mark Verbeek <markverbeek@mac.com>
2020-07-15 19:27:20 +0200

    campground authorization

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -61,19 +61,15 @@ router.get("/:id", function (req, res) {
 });
 
 //EDIT CAMPGROUND ROUTE
-router.get("/:id/edit", function (req, res) {
+router.get("/:id/edit", checkCampgroundOwnership, function (req, res) {
     Campground.findById(req.params.id, function (err, foundCampground) {
-        if (err) {
-            res.redirect("/campgrounds")
-        } else {
-            res.render("campgrounds/edit", { campground: foundCampground });
-        }
+        res.render("campgrounds/edit", { campground: foundCampground });
     });
 });
 
 //UPDATE CAMPGROUND ROUTE
 
-router.put("/:id", function (req, res) {
+router.put("/:id", checkCampgroundOwnership, function (req, res) {
     Campground.findByIdAndUpdate(req.params.id, req.body.campground, function (err, updatedCampground) {
         if (err) {
             res.redirect("/campgrounds");
@@ -84,7 +80,7 @@ router.put("/:id", function (req, res) {
 })
 
 //DESTROY CAMPGROUND ROUTE
-router.delete("/:id", function (req, res) {
+router.delete("/:id", checkCampgroundOwnership, function (req, res) {
     Campground.findByIdAndRemove(req.params.id, function (err) {
         if (err) {
             res.redirect("/campgrounds");
@@ -101,6 +97,24 @@ function isLoggedIn(req, res, next) {
     }
     res.redirect("/login");
 }
+
+function checkCampgroundOwnership(req, res, next) {
+    if (req.isAuthenticated()) {
+        Campground.findById(req.params.id, function (err, foundCampground) {
+            if (err) {
+                res.redirect("back")
+            } else {
+                if (foundCampground.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
+};
 
 
 module.exports = router
